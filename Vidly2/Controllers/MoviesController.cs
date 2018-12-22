3746dbe1424e18057a46dff31496728420d3fb27
eszja <a.juhasz.mail@gmail.com>
2018-12-22 14:31:44 +0100

    first commit - Vidly 2

---
+++ Vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Matrix" };
+            return View(movie);
+        }
+    }
+}
